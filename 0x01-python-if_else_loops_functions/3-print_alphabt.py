282b6877ea5d19ea7464aa9a8f42ba286f78ff9c
Lex9jar <alexanderedim89@gmail.com>
2024-01-17 00:11:28 +0100

    Prints the alphabets in lowercase except 'q' and 'e', not followed by a new line.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Prints the ASCII alpabet, in lowercase except 'q' and 'e', not followed by a new line."""
 
-for char in range(97, 122):
+for char in range(97, 123):
     if chr(char) != 'q' and chr(char) != 'e':
         print("{}".format(chr(char)), end="")
