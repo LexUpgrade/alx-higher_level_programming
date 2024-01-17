f53f70a24758da6d3dca4cc06680b3ce9905a528
Lex9jar <alexanderedim89@gmail.com>
2024-01-17 20:03:29 +0100

    Performs an operation

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Alexander Edim
+
+def magic_calculation(a, b, c):
+    """Matches bytecode provided by ALX SE"""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a * b - )
