d9b4146fcd916e9a38ce3447a569a31906c53868
Lex9jar <alexanderedim89@gmail.com>
2024-01-19 00:48:43 +0100

    A simple interactive calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == '__main__':
-    """A simple calculator"""
-    import sys
+    """A simple interactive calculator."""
     from calculator_1 import add, sub, mul, div
+    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -10,7 +11,7 @@ if __name__ == '__main__':
 
     opr = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(opr.keys()):
-        print("Unknwon operator. Available operators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
