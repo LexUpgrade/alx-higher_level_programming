199c47e4b758a45e7d38e9808e2b196b5bcb2af4
Lex9jar <alexanderedim89@gmail.com>
2024-01-18 22:55:39 +0100

    Print all names defined by hidden_4 module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ if __name__ = '__main__':
     """Prints all the names defined by a compiled module"""
     import hidden_4
 
-    names = dev(hidden_4)
+    names = dir(hidden_4)
     for name in names:
         if name[:2] != '__':
             print(name)
