92a196296e7b2e6a8a28c833384bd058c1bf6ad2
Lex9jar <alexanderedim89@gmail.com>
2024-01-19 00:06:27 +0100

    A simple interactive calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,16 +4,15 @@ if __name__ == '__main__':
     import sys
     from calculator_1 import add, sub, mul, div
 
-    count = len(sys.argv) - 1
-    if count != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    opr = {'+': add, '-': sub, '*': mul, '/': div}
-    if sys.argv[2] not in opr:
+    opr = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(opr.keys()):
         print("Unknwon operator. Available operators: +, -, * and /")
         sys.exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2]](a, b)))
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, opr[sys.argv[2]](a, b)))
