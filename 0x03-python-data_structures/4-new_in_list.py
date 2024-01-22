b7807cdcafd94a056f5319e568f78927f90cc6f5
Lex9jar <alexanderedim89@gmail.com>
2024-01-22 19:20:54 +0100

    Replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position
+        without modifying the original list
+
+    Args:
+        my_list: Original list
+        idx: Index to modify
+        element: Element for replacement
+
+    Returns:
+        None if idx is negative or out of bound, otherwise
+        newly modified list
+    """
+
+    count = len(my_list) - 1
+    if idx < 0 or idx > count:
+        return None
+    else:
+        new_list = [0 for i in range(len(my_list))]
+        for i in range(len(my_list)):
+            if i == idx:
+                new_list[i] = element
+            else:
+                new_list[i] = my_list[i]
+        return (new_list)
