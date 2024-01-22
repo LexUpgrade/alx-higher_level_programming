87878755046c5589c56a3e2749319cae8c9a11cb
Lex9jar <alexanderedim89@gmail.com>
2024-01-22 18:48:50 +0100

    Replaces an element of a list at a specific position

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replaces an element of a list at a specific index
+
+    Args:
+        my_list: List to modify
+        idx: Index to overwrite
+        element: Element for replacement
+
+    Return:
+        None if idx is negative or out of bound, otherwise
+        newly modified list
+    """
+
+    count = len(my_list) - 1
+    if idx < 0 or idx > count:
+        return None
+    else:
+        my_list[idx] = element
+
+    return (my_list)
