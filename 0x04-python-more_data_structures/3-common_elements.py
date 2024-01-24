b4b4bcd22828b6736f97456e60b0a9b8202cee5d
Lex9jar <alexanderedim89@gmail.com>
2024-01-24 22:32:41 +0100

    Returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """Returns a set of common elements in two sets.
+
+    Args:
+        set_1: first set
+        set_2: second set
+
+    Returns:
+        A set of common attributes of set_1 and set_2
+    """
+
+    return set_1 & set_2
