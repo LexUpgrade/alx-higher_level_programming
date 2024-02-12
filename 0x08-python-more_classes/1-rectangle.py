4bca28f27d6428e52dc7c51ed440ea83bf8fe44a
Lex9jar <alexanderedim89@gmail.com>
2024-02-12 22:18:10 +0100

    Added properties for initializing an instance of class Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,34 +11,32 @@ class Rectangle:
         Args:
             width (int): Width of the rectangle
             height (int): Height of the rectangle
-
         """
-
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """Get/set attribute width."""
-            return self.__width
+    @property
+    def width(self):
+        """Get/set attribute width."""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """Get/set attribute height."""
-            return self.__height
+    @property
+    def height(self):
+        """Get/set attribute height."""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("height must be an integre")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integre")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
