f72a0e312e746ee2beb68ce8b7bf31ed12ae1fec
Lex9jar <alexanderedim89@gmail.com>
2024-02-12 22:31:36 +0100

    Added a public instance method area() and perimeter()

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Returns a rectangula perimeter."""
+        """Returns the perimeter of a Retangle instance."""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
