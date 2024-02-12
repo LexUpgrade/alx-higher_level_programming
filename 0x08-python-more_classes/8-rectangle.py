b63d94a38885b88cb2656ed842c1247e18a5998f
Lex9jar <alexanderedim89@gmail.com>
2024-02-13 00:31:01 +0100

    Added a static method to return the biggest rectangle based on the area

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -103,7 +103,4 @@ class Rectangle:
             raise TypeError("ract_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        elif rect_1.area() > rect_2.area():
-            return rect_1
-        else:
-            return rect_2
+        return rect_2
