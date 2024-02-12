fb71420be805b9fc33943199d469527da0eb3261
Lex9jar <alexanderedim89@gmail.com>
2024-02-12 22:07:44 +0100

    Added properties for initializing an instance of class Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,11 +22,6 @@ class Rectangle:
             """Get/set attribute width."""
             return self.__width
 
-        @proerty
-        def height(self):
-            """Get/set attribute height."""
-            return self.__height
-
         @width.setter
         def width(self, value):
             if not isinstance(value, int):
@@ -34,6 +29,11 @@ class Rectangle:
             elif value < 0:
                 raise ValueError("width must be >= 0")
             self.__width = value
+
+        @property
+        def height(self):
+            """Get/set attribute height."""
+            return self.__height
 
         @height.setter
         def height(self, value):
