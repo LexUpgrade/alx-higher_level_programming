45106455d1aad4e20341387567afcc89ce140c2d
Lex9jar <alexanderedim89@gmail.com>
2024-02-12 21:55:57 +0100

    Added properties for initializing an instance of class Rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Rectangle:
+    """A class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Creates an instance of class Rectangle
+
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+
+        """
+
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """Get/set attribute width."""
+            return self.__width
+
+        @proerty
+        def height(self):
+            """Get/set attribute height."""
+            return self.__height
+
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integre")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
