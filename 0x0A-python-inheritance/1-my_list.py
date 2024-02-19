d533b020239727cd9dd9fa4059aa9da509ced02f
Lex9jar <alexanderedim89@gmail.com>
2024-02-19 22:21:06 +0100

    Prints a sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,2 @@
-#!/urs/bin/python3
-"""Defines a class."""
+#!/usr/bin/python3
 
-
-class MyList(list):
-    """A class that inherits from list.
-
-    Args:
-        list: A base class.
-    """
-
-    def __init__(self):
-        """Initializes the object."""
-        super().__init__()
-
-    def print_sorted(self):
-        """Prints a sorted list."""
-        print(sorted(self))
