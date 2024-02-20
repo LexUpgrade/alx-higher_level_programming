8fc9edf8d5d6207bf6347f7f4380f04a1ffd5f9f
Lex9jar <alexanderedim89@gmail.com>
2024-02-20 21:48:18 +0100

    Modified 'to_json'

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a class 'Student'."""
+
+
+class Student:
+    """A Student class."""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance of Student.
+
+        Args:
+            first_name (str): The student's first name
+            last_name (str): The student's last name
+            age (int): The student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrives a dictionary representation of a 'Student' instance.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
