df9e919f5affcd3d51920ba918f64c3bb46dae2a
Lex9jar <alexanderedim89@gmail.com>
2024-02-27 00:34:35 +0100

    A Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Defines a <Rectangle> class."""
 from models.base import Base
 
+
 class Rectangle(Base):
-    """A class <rectangle that inherits from <Base>."""
+    """A class <rectangle> that inherits from <Base>."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Instantiates an instance of a <Rectangle>
