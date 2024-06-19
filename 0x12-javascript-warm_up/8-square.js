8ac919ee1906b09cda6f194ce575367a43ef147f
Lex9jar <alexanderedim89@gmail.com>
2024-06-19 22:22:14 +0100

    Prints a square in the size passed as an argument to it

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   const x = parseInt(process.argv[2]);
   let i;
