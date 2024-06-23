0f55dee03a2e31e19eff8ca41e598bd3fa18f73e
Lex9jar <alexanderedim89@gmail.com>
2024-06-23 21:39:06 +0100

    Lists all 'states' with 'name' starting with 'N' from the database 'hbtn_0e_0_usa'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Lists all 'states' with 'name' starting with 'N' from the database
+    'hbtn_0e_0_usa'.
+"""
+from sys import argv as av
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=av[1], passwd=av[2], db=av[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * \
+                    FROM states \
+                    WHERE name LIKE ("N%") \
+                    ORDER BY id \
+                    """)
+    states = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    for state in states:
+        print(state)
