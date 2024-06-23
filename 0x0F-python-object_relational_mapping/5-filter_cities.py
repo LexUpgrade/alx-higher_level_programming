50ff58da20024c9f25d6a332af4e00a40bf4f994
Lex9jar <alexanderedim89@gmail.com>
2024-06-24 00:49:26 +0100

    Takes in the name of a state as an argument and lists all 'cities' of that state, using the database: 'hbtn_0e_0_usa' (Safe from MySQL Injections)

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,17 +11,13 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=av[1], passwd=av[2], db=av[3])
     cur = db.cursor()
-    cur.execute("""SELECT c.name FROM states s
-                JOIN cities c ON s.id LIKE c.state_id
-                WHERE c.state_id LIKE
-                (SELECT states.id FROM states WHERE name LIKE %s)
-                ORDER BY c.id
+    cur.execute("""SELECT c.name FROM cities c
+                INNER JOIN states s ON s.id=c.state_id
+                WHERE s.name=%s
                 """, (av[4], ))
     cities = cur.fetchall()
     cur.close()
     db.close()
 
-    for i, city in enumerate(cities):
-        print(city[0], end="")
-        if i != len(cities) - 1:
-            print(", ", end="")
+    cities_ = [c[0] for c in cities]
+    print(", ".join(cities_))
