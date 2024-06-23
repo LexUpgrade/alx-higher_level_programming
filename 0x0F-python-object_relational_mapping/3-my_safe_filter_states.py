e9ce593f098b1bbae8a7b7d4096f4816cd0c90c7
Lex9jar <alexanderedim89@gmail.com>
2024-06-23 23:25:45 +0100

    Takes in arguments and displays all values in the 'states' table of 'hbtn_0e_0_usa' where 'name' matches the argument. (Safe from MySQL Injections)

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Takes in arguments and displays all values in the 'states' table of
+    'hbtn_0e_0_usa' where 'name' matches the argument. (Safe from MySQL
+    injections.
+"""
+from sys import argv as av
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=av[1], passwd=av[2], db=av[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (av[4], ))
+    states = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    for state in states:
+        print(state)
