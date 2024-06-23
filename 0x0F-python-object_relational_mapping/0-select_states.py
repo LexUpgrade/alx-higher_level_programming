55b848893c5c68af9ee17573cc9f0fc8d2baddb1
Lex9jar <alexanderedim89@gmail.com>
2024-06-23 21:20:22 +0100

    Lists all 'states' from the database 'hbtn_0e_0_usa'

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,19 +3,20 @@
 from sys import argv
 import MySQLdb
 
-usr = argv[1]
-pswrd = argv[2]
-db_name = argv[3]
+if __name__ == '__main__':
+    usr = argv[1]
+    pswrd = argv[2]
+    db_name = argv[3]
 
-db = MySQLdb.connect(host='localhost', port=3306,
+    db = MySQLdb.connect(host='localhost', port=3306,
                      user=usr, passwd=pswrd, db=db_name)
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states;")
-states = cursor.fetchall()
+    cursor.execute("SELECT * FROM states;")
+    states = cursor.fetchall()
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
 
-for state in states:
-    print(state)
+    for state in states:
+        print(state)
