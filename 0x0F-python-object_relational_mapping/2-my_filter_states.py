aaee04846ca054f7720e48f937b62f23fd8cf156
Lex9jar <alexanderedim89@gmail.com>
2024-06-23 22:39:56 +0100

    Takes an argument and displays all values in the 'states' table of 'hbtn_0e_0_usa' where 'name' matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Takes in an argument and displays all values in the 'states' table of
+    'hbtn_0e_0_usa' where 'name' matches the argument.
+"""
+from sys import argv as av
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=av[1], passwd=av[2], db=av[3])
+    cursor = db.cursor()
+    cursor.execute(f"""SELECT *
+                        FROM states
+                        WHERE name LIKE BINARY ("{av[4]}")
+                        ORDER BY states.id
+                        """)
+    states = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    for state in states:
+        print(state)
