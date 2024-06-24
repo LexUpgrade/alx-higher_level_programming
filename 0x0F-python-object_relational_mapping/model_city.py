8e4b40e9948a52b2a665ed8345cac766e4be8609
Lex9jar <alexanderedim89@gmail.com>
2024-06-24 03:20:12 +0100

    Class Model 'City'

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Creates a class 'City'.
+"""
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+
+Base = declarative_base()
+class City(Base):
+    """A class 'City'.
+
+    Args:
+        id (int): ID of the city
+        name (string): Name of the city
+        state_id (int): ID of the referencing state
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("State.id"), nullable=False)
