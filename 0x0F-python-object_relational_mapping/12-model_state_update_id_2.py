9c03b46b2cd4d0f40e9e53a101c1098329e40844
Lex9jar <alexanderedim89@gmail.com>
2024-06-24 02:36:32 +0100

    Changes the name of a 'State' object from the database 'hbtn_0e_6_usa'

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Changes the name of a 'State' object from the database 'hbtn_0e_6_usa'.
+"""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv as av
+
+if __name__ == "__main__":
+    url_db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(av[1], av[2], av[3])
+    engine = create_engine(url_db)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    mod_state = session.query(State).where(State.id == 2).one_or_none()
+    if mod_state:
+        mod_state.name = "New Mexico"
+        session.commit()
