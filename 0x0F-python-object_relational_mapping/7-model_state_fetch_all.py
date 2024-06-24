fd41927aea0ddaf54d84d458dda43a09fceac035
Lex9jar <alexanderedim89@gmail.com>
2024-06-24 01:53:20 +0100

    Lists all 'State' objects from the database 'hbtn_0e_6_usa'

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Lists all 'State' objects from the database 'hbtn_0e_6_usa'."""
 from sys import argv as av
-from model_state import State
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-url_db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(av[1], av[2], av[3])
-engine = create_engine(url_db)
-Session = sessionmaker(bind=engine)
-session = Session()
+if __name__ == "__main__":
+    url_db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(av[1], av[2], av[3])
+    engine = create_engine(url_db)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-states = session.query(State).all()
+    states = session.query(State).all()
 
-[print("{}: {}".format(state.id, state.name)) for state in states]
+    [print("{}: {}".format(state.id, state.name)) for state in states]
