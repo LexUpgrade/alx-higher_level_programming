fa6cc9d07df8e3b977276fa5559c501bf75d977c
Lex9jar <alexanderedim89@gmail.com>
2024-06-24 02:12:52 +0100

    Prints the 'State' object with the 'name' passed as argument from the database 'hbtn_0e_6_usa'

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Prints the 'State' object with the 'name' passed as argument from the
+    database 'hbtn_0e_6_usa'.
+"""
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv as av
+
+if __name__ == "__main__":
+    url_db = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(av[1], av[2], av[3])
+    engine = create_engine(url_db)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(name=av[4]).one_or_none()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
