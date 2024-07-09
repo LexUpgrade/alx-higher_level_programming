2028ff078154dfcae66fa57ab396e2328dd0a4d6
Lex9jar <alexanderedim89@gmail.com>
2024-07-09 17:52:22 +0100

    Takes a URL and sends a request to the URL and displays the value of the variable 'X-Request-Id' in the response header.

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes a URL and sends a request to the URL and displays
+    the value of the variable 'X-Request-Id' in the response
+    header.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    response = requests.get(url)
+    print("{}".format(response.headers['X-Request-Id']))
