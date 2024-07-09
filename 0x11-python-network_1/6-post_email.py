8b52d5ade696b1c64103695484c724247ace9ac4
Lex9jar <alexanderedim89@gmail.com>
2024-07-09 18:07:53 +0100

    Takes a URL and an email address, and sends a POST request to the passed URL with the email address as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Takes a URL and an email address, and sends a POST request to the passed URL
+    with the email address as a parameter, and finally displays the body of the
+    response.
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
